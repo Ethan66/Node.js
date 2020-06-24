f47fd4465a3abb02dbfd9c26af918f66222df8df
zhangcheng <zhuangcheng@ldxinyong.com>
2020-06-24 17:03:54 +0800

    update

--- demo1/http.js
+++ demo1/http.js
@@ -1,4 +1,4 @@
-let http = require('http')
+/* let http = require('http')
 let fs = require('fs')
 let path = require('path')
 let url = require('url')
@@ -38,4 +38,49 @@ function getMime (extname) {
     default:
       return 'text/html'
   }
-}
+} */
+
+const url = require('url')
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
+
+http.createServer((req, res) => {
+  let pathname = url.parse(req.url, true).pathname
+  if (pathname === '/') {
+    pathname = '/index.html'
+  }
+  let extname = path.extname(pathname)
+  let type = ''
+  switch(extname) {
+    case '.html':
+      type = 'text/html'
+      break
+    case '.css':
+      type = 'text/css'
+      break
+    case '.js':
+      type = 'text/javascript'
+      break
+  }
+  res.writeHead(200, { "content-Type":  type + ";charset='utf-8'" })
+  if (pathname === '/favicon.ico') {
+    res.end()
+    return false
+  }
+  fs.readFile(`./demo1/${pathname}`, (err, data) => {
+    if (err) {
+      fs.readFile('./demo1/404.html', (err, data) => {
+        if (err) {
+          console.log(err)
+          return false
+        }
+        res.write(data)
+        res.end()
+      })
+      return false
+    }
+    res.write(data)
+    res.end()
+  })
+}).listen(8001)
